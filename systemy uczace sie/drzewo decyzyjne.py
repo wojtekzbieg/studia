387107be9c4d487c28c55a9f04425f8011c9dddf
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-10-20 16:49:23 +0200

    work in progress

--- systemy uczace sie/drzewo decyzyjne.py
+++ systemy uczace sie/drzewo decyzyjne.py
@@ -17,30 +17,50 @@ def wczytaj_plik(plik):
 
     return lista
 
-lista = wczytaj_plik("gieldaLiczby.txt")
-print(lista)
-zbior1 = {}
-zbior2 = {}
+def oblicz_liczbe_wartosci(lista):
+    zbior1 = {}
+    for j in range(len(lista[0])):
+        lista2=[]
+        for i in range(len(lista)):
+            lista2.append(lista[i][j])
+        wartosci = set(lista2)
+        # print(f"Atrybut na pozycji {j} - {len(wartosci)} mozliwe wartosci")
+        zbior1[j]= len(wartosci)
+    return zbior1
+
+def oblicz_liczbe_wystapien_wartosci(lista):
+    zbior2 = {}
+    for j in range(len(lista[0])):
+        lista2 = []
+        for i in range(len(lista)):
+            lista2.append(lista[i][j])
+        wartosci = set(lista2)
+
+        lista_wystapien = []
+        for k in wartosci:
+            liczba_wystapien = lista2.count(k)
+            # print(f"Wartosc atrybutu: {k}, liczba wystapien: {liczba_wystapien}")
+            zbior3= {k: liczba_wystapien}
+            print(zbior3)
+            lista_wystapien.append(zbior3)
+        zbior2[j] = lista_wystapien
+    return zbior2
 
 
-for j in range(len(lista[0])):
-    lista2=[]
-    for i in range(len(lista)):
-        lista2.append(lista[i][j])
-    wartosci = set(lista2)
-    print(f"Atrybut na pozycji {j} - {len(wartosci)} mozliwe wartosci")
-    zbior1[j]= len(wartosci)
+lista_z_plikiem = wczytaj_plik("gieldaLiczby.txt")
 
-    lista_wystapien = []
-    zbior3 = {}
-    for k in wartosci:
-        liczba_wystapien = lista2.count(k)
-        print(f"Wartosc atrybutu: {k}, liczba wystapien: {liczba_wystapien}")
-        zbior3= {k: liczba_wystapien}
-        lista_wystapien.append(zbior3)
-    zbior2[j] = lista_wystapien
+liczba_wartosci = oblicz_liczbe_wartosci(lista_z_plikiem)
+liczba_wystapien_wartosci = oblicz_liczbe_wystapien_wartosci(lista_z_plikiem)
 
-    print()
+# print(liczba_wartosci)
+# print(liczba_wystapien_wartosci)
 
-print(zbior1)
-print(zbior2)
+
+# liczba_wystapien_wartosci = liczba_wystapien_wartosci.get(len(liczba_wystapien_wartosci)-1)
+# print(liczba_wystapien_wartosci[0].get)
+
+
+
+
+
+
