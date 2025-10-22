62f5a1e5ed56b8d8615065f106e83d478789de8a
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-10-22 10:14:31 +0200

    modularyzacja

--- zaawansowane programowanie/modularyzacja.py
+++ zaawansowane programowanie/modularyzacja.py
@@ -4,6 +4,8 @@ from pakiet_testowy import module1
 from pakiet_testowy.module1 import  b
 from pakiet_testowy.module1 import potegowanie
 
+import lista_jedno_i_dwukierunkowa
+
 print(module1.a)
 print(b)
 print(potegowanie(3,2))
@@ -15,3 +17,8 @@ print(module1.Pies.ile_lap())
 print(module1.Pies.zaszczekaj())
 
 # print(module2.logarytm_binarny(2))
+
+
+lista1 = lista_jedno_i_dwukierunkowa.ListaJednokierunkowa()
+lista1.dodaj_na_poczatku(5)
+lista1.wyswietl_od_poczatku()
