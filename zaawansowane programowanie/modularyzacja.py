5f9d5444523ea1698b977a4eb5bbe0d04a8c4044
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-10-22 09:05:30 +0200

    modularyzacja

---
+++ zaawansowane programowanie/modularyzacja.py
@@ -0,0 +1,12 @@
+import pakiet_testowy
+from pakiet_testowy import *
+from pakiet_testowy import module1
+from pakiet_testowy.module1 import  b
+from pakiet_testowy.module1 import potegowanie
+
+print(module1.a)
+print(b)
+print(potegowanie(3,2))
+print(module1.pierwiastkowanie(9,2))
+
+print(module2.logarytm_binarny(2))
---
+++ zaawansowane programowanie/pakiet_testowy/module1.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+
+def potegowanie(x,y):
+    return x ** y
+
+def pierwiastkowanie(x,y):
+    return x ** (1/y)
