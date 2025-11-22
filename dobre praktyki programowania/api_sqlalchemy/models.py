afb0c7bd69fb1c55aa25e9f7e2f2a29f4e31cace
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-11-22 18:09:24 +0100

    added Users table

--- dobre praktyki programowania/api_sqlalchemy/models.py
+++ dobre praktyki programowania/api_sqlalchemy/models.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-from typing import Union
 from sqlalchemy import Column, Integer, String, Numeric
 from sqlalchemy.orm import declarative_base
 
@@ -47,3 +46,11 @@ class Link(Base):
     movieId = Column(Integer, primary_key=True)
     imdbId = Column(Integer)
     tmdbId = Column(Integer)
+
+
+class User(Base):
+    __tablename__ = "Users"
+
+    userId = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(100))
+    hashed_password = Column(String(200))
