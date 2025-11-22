29416c25b568903c057563a55881e3fca5569ca5
wojtekzbieg <wojtekzbieg56@gmail.com>
2025-11-22 20:55:43 +0100

    token creation and validation

--- dobre praktyki programowania/api_sqlalchemy/main.py
+++ dobre praktyki programowania/api_sqlalchemy/main.py
@@ -1,8 +1,10 @@
 from typing import Annotated
-from fastapi import FastAPI
 from connection import session
-from models import Item, Movie, Tag, Rating, Link
+from models import Item, Movie, Tag, Rating, Link, User
+from fastapi import FastAPI
 # import csv
+import jwt
+from datetime import datetime, timedelta
 
 
 app = FastAPI()
@@ -46,4 +48,20 @@ def wyswietl_linki():
     lista_linkow = session.query(Link).all()
     return lista_linkow
 
+
+klucz="asnfj46fsdvtd5fg"
+algorytm="HS256"
+
+def stworz_token(payload={"sub": "Wojtek", "expiration": str(datetime.now() + timedelta(minutes=30))}):
+    JWT_token_encoded = jwt.encode(payload=payload, key=klucz, algorithm=algorytm)
+    return JWT_token_encoded
+
+
+def zdekoduj_token(token):
+    JWT_token_decoded = jwt.decode(jwt=token, key=klucz, algorithms=algorytm)
+    return JWT_token_decoded
+
+token = stworz_token()
+print(token)
+print(zdekoduj_token(token))
 
